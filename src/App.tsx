87f725783e9574f885ddf70eb7366018ae7c0a89
MiguelDias <miguel.dias@estudante.ufjf.br>
2024-10-11 16:38:33 -0300

    input adicionado ao game select element

--- src/App.tsx
+++ src/App.tsx
@@ -6,28 +6,22 @@ import Footer from './Componentes/Footer/Footer.jsx'
 import Question from './Componentes/Question/Question.jsx'
 import SelectGame from './Componentes/SelectGame/SelectGame.jsx'
 import questionApi from './Controlers/QuestionApi.js'
-import Resultado from './Resultado/Resultado.tsx'
 function App() {
   const [count, setCount] = useState(0)
   const questoes = questionApi.questions
-  let terminou: boolean = false;
   return (
     <div className='App'>
       <Header></Header>
       
       <main>
-        
-        {
+        <SelectGame></SelectGame>
+        {/* {
             questoes.map((questao) => {
-
-              
               return (
                 <Question nome={questao.nome} options={questao.options}></Question>
               )
             }) 
-          
-          
-        }
+        } */}
       </main>
       <Footer></Footer>
     </div>
--- src/Componentes/SelectGame/SelectGame.tsx
+++ src/Componentes/SelectGame/SelectGame.tsx
@@ -3,8 +3,15 @@ import { useState } from 'react'
 import './SelectGame.css'
 import './Slider.css'
 function SelectGame() {
-  const [count, setCount] = useState(0)
-
+    const [range1, setRange1] = useState(0)
+    const [range2, setRange2] = useState(0)
+    const [range3, setRange3] = useState(0)
+    const [range4, setRange4] = useState(0)
+    const [range5, setRange5] = useState(0)
+    
+    function handleChange(event: any, setRange1: any) {
+        setRange1(event.target.value)
+    }
   return (
     <div className='GameSelectCtn'>
         <h2>Selecione o Tipo de Jogador pro seu jogo</h2>
@@ -12,32 +19,28 @@ function SelectGame() {
         <section>
             <ul>
                 <li>
-                    <input type="radio" id="html" name="fav_language" value="HTML"></input>
-                    <label htmlFor={"html"}>Acrobat</label>
+                <input onChange={(e)=>{handleChange(e,setRange1)}} type="range"  min="0" max="100"   /> 
+                            <label >{"Char1: "+range1}</label>
                 </li>
                 <li>
-                    <input type="radio" id="html" name="fav_language" value="HTML"></input>
-                    <label htmlFor={"html"}>HTML</label>
+                    <input onChange={(e)=>{handleChange(e,setRange2)}} type="range" id="html" name="fav_language" ></input>
+                    <label >{range2}</label>
                 </li>
                 <li>
-                    <input type="radio" id="html" name="fav_language" value="HTML"></input>
-                    <label htmlFor={"html"}>HTML</label>
+                    <input onChange={(e)=>{handleChange(e,setRange3)}} type="range" id="html" name="fav_language" ></input>
+                    <label >{range3}</label>
                 </li>
+                <li>
+                    <input onChange={(e)=>{handleChange(e,setRange4)}} type="range" id="html" name="fav_language" ></input>
+                    <label >{range4}</label>
+                </li>
+                <li>
+                    <input onChange={(e)=>{handleChange(e,setRange5)}} type="range" id="html" name="fav_language" ></input>
+                    <label >{range5}</label>
+                </li>
+                
             </ul>
-            <ul>
-                <li>
-                    <input type="radio" id="html" name="fav_language" value="HTML"></input>
-                    <label htmlFor={"html"}>HTML</label>
-                </li>
-                <li>
-                    <input type="radio" id="html" name="fav_language" value="HTML"></input>
-                    <label htmlFor={"html"}>HTML</label>
-                </li>
-                <li>
-                    <input type="radio" id="html" name="fav_language" value="HTML"></input>
-                    <label htmlFor={"html"}>HTML</label>
-                </li>
-            </ul>
+           
         </section>
       
                         
