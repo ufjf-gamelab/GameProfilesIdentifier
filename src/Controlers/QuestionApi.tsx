02021c256183cc545ce4be42f83712b9b298cc28
Bebs Fiorillo <estevaovrb2014@gmail.com>
2024-10-11 11:46:23 -0300

    comeco declaracao (jesus)

--- src/Controlers/QuestionApi.tsx
+++ src/Controlers/QuestionApi.tsx
@@ -1,16 +1,27 @@
+class opcao{
+    resposta: string;
+    propiedades: [number, number, number, number, number];
+}
+
+
 interface Question {
     nome: string;
-    options: string[];
+    options: opcao[];
     selected?: number;
 }
 
 class QuestionApi {
     questions: Question[] = [];
 
-    addQuestion(nome: string, Option: string[]) {
+    addQuestion(nome: string, textoOpcao: string[], resposta: [number, number, number, number, number]) {
         this.questions.push({
-            nome: nome,
-            options: Option
+            nome: nome;
+            for (index = 0; index < textoOpcao.length; index++) {
+                options: {
+                    resposta: textoOpcao[index];
+                    propiedades: resposta;
+                }
+            }
         });
     }
     constructor() {
@@ -20,7 +31,7 @@ class QuestionApi {
 
 let questionApi = new QuestionApi();
 for (let index = 0; index < 5; index++) {
-    questionApi.addQuestion("Teste "+index, ["a", "b","c","d"]);
+    questionApi.addQuestion("Teste " + index, ["a", [1, 2, 3, 4, 5]]);
 
     
 }
