c9c8aeae50475faa59342a6ccf2bed705f264491
Bebs Fiorillo <estevaovrb2014@gmail.com>
2024-10-11 12:06:44 -0300

    addQuestion funcional (talvez?)

--- src/Controlers/QuestionApi.tsx
+++ src/Controlers/QuestionApi.tsx
@@ -1,4 +1,4 @@
-class opcao{
+interface opcao{
     resposta: string;
     propiedades: [number, number, number, number, number];
 }
@@ -13,16 +13,21 @@ interface Question {
 class QuestionApi {
     questions: Question[] = [];
 
-    addQuestion(nome: string, textoOpcao: string[], resposta: [number, number, number, number, number]) {
-        this.questions.push({
-            nome: nome;
-            for (index = 0; index < textoOpcao.length; index++) {
-                options: {
-                    resposta: textoOpcao[index];
-                    propiedades: resposta;
-                }
-            }
-        });
+    addQuestion(nome: string, textoOpcao: string[], propsOpcao: [number, number, number, number, number][]) {
+        let OpcoesQuest: opcao[] = [];
+        for (let i=0; i< textoOpcao.length; i++){
+            let optionDefinir: opcao = {
+                resposta: textoOpcao[i],
+                propiedades: propsOpcao[i] as [number, number, number, number, number],
+            };
+            OpcoesQuest.push(optionDefinir);
+        }
+        
+        let NovaQuestao: Question = {
+            nome: nome,
+            options: OpcoesQuest,
+        };
+        this.questions.push(NovaQuestao);
     }
     constructor() {
         
@@ -31,8 +36,7 @@ class QuestionApi {
 
 let questionApi = new QuestionApi();
 for (let index = 0; index < 5; index++) {
-    questionApi.addQuestion("Teste " + index, ["a", [1, 2, 3, 4, 5]]);
-
+    //questionApi.addQuestion("Teste " + index, ["a", [1, 2, 3, 4, 5]]);
     
 }
 export default questionApi;
