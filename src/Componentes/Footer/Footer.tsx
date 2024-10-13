f401cbc34f6daf4a0f887a49ead369a8369a4c60
MiguelDias <miguel.dias@estudante.ufjf.br>
2024-10-13 02:54:36 -0300

    Adiciona os pesos corretos

--- src/Componentes/Footer/Footer.tsx
+++ src/Componentes/Footer/Footer.tsx
@@ -8,11 +8,21 @@ function App() {
   return (
     <footer>
         <img src={inova}></img>
-        <section>
+        <section style={{marginTop:'10px'}}>
             <h2>Desenvolvido por:</h2>
             <h3>Tevo Fiorilo</h3>
             <h3>Miguel Dias</h3> 
             <h3> Isabela Coelho</h3>        
+        </section>
+        <section style={{marginTop:'10px'}}>
+          <h2>Referências:</h2>
+            <a  style={{color:'white'}}href='https://quanticfoundry.com/gamer-types/'>The 9 Quantic Gamer Types</a>     
+            <a  style={{color:'white'}}href='https://apps.quanticfoundry.com/surveys/start/gamerprofile/'>gamerprofile Quantic Foundry</a> 
+            <a  style={{color:'white'}}href='https://quanticfoundry.com/'>Quantic Foundry</a>
+            <a  style={{color:'white'}}href=' https://mud.co.uk/richard/selfware.html '>A Self of Sense</a>
+
+            <a  style={{color:'white'}}href='https:www.google.com/'>Nosso Texto Não lembro o titulo</a>      
+
         </section>
     </footer>
   )
--- src/Controlers/ResultGameApi.tsx
+++ src/Controlers/ResultGameApi.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 type Motivações = {ação:number, social : number, maestria : number, conquista : number, imersão : number, criatividade : number};
+
 class Perfil{
     nome: string;
     #agradabilidade: number = 0;
@@ -26,25 +27,25 @@ class Perfil{
 
 class ResultApi {
     Inputs: Motivações = {ação:10, social : 10, maestria : 10, conquista : 10, imersão : 10, criatividade : 10};
-    Agradabilidades: Perfil[] = [
-        new Perfil(this.Inputs,{ação:1, social : 1, maestria : 1, conquista : 1, imersão : 1, criatividade : 1},'Acrobata'),
-        new Perfil(this.Inputs,{ação:1, social : 1, maestria : 1, conquista : 1, imersão : 1, criatividade : 1},'Jardineiro'),
-        new Perfil(this.Inputs,{ação:1, social : 1, maestria : 1, conquista : 1, imersão : 1, criatividade : 1},'Caçador'),
-        new Perfil(this.Inputs,{ação:1, social : 1, maestria : 1, conquista : 1, imersão : 1, criatividade : 1},'Brigão'),
-        new Perfil(this.Inputs,{ação:1, social : 1, maestria : 1, conquista : 1, imersão : 1, criatividade : 1},'Gladiador'),
-        new Perfil(this.Inputs,{ação:1, social : 1, maestria : 1, conquista : 1, imersão : 1, criatividade : 1},'Ninja'),
-        new Perfil(this.Inputs,{ação:1, social : 1, maestria : 1, conquista : 1, imersão : 1, criatividade : 1},'Caçador \n d   e Recompensas'),
-        new Perfil(this.Inputs,{ação:1, social : 1, maestria : 1, conquista : 1, imersão : 1, criatividade : 1},'Arquiteto'),
-        new Perfil(this.Inputs,{ação:1, social : 4, maestria : 1, conquista : 1, imersão : 1, criatividade : 1},'Bardo')
+    Agradabilidades: Perfil[] = [ //social  //ação // conquista // maestria // imersão // criatividade
+        new Perfil(this.Inputs,{ação:89, social : 62, maestria : 70, conquista : 62, imersão : 21, criatividade : 64},'Acrobata'),
+        new Perfil(this.Inputs,{ação:54, social : 85, maestria : 41, conquista : 89, imersão : 43, criatividade : 50},'Jardineiro'),
+        new Perfil(this.Inputs,{ação:50+23, social : 35+26, maestria : 27+14, conquista : 23+17, imersão : 62+58, criatividade : 18+14},'Caçador'),
+        new Perfil(this.Inputs,{ação:78+27, social : 71+78, maestria : 37+39, conquista : 27+66, imersão : 30+12, criatividade : 37+37},'Brigão'),
+        new Perfil(this.Inputs,{ação:69+75, social : 76+76, maestria : 68+79, conquista : 78+74, imersão : 61+62, criatividade : 66+63},'Gladiador'),
+        new Perfil(this.Inputs,{ação:74+50, social : 56+82, maestria : 69+76, conquista : 22+49, imersão : 27+43, criatividade : 15+22},'Ninja'),
+        new Perfil(this.Inputs,{ação:61+75, social : 24+28, maestria : 28+31, conquista : 45+61, imersão : 70+63, criatividade : 61+62},'Caçador \n d   e Recompensas'),
+        new Perfil(this.Inputs,{ação:15+23, social : 20+23, maestria : 63+35, conquista : 59+52, imersão : 59+59, criatividade : 51+51},'Arquiteto'),
+        new Perfil(this.Inputs,{ação:35+34, social : 68+37, maestria : 52+32, conquista : 26+18, imersão : 62+67, criatividade : 66+69},'Bardo')
     ];
     
     get Nomes(){
         return this.Agradabilidades.map((p) => p.nome);
     }
     get Resultados(){
-        Array.prototype.sort.call(this.Agradabilidades, (a:Perfil, b:Perfil) => {
-            return b.agradabilidade - a.agradabilidade;
-        } );
+        // Array.prototype.sort.call(this.Agradabilidades, (a:Perfil, b:Perfil) => {
+        //     return b.agradabilidade - a.agradabilidade;
+        // } );
         return this.Agradabilidades
     }
     constructor() {
