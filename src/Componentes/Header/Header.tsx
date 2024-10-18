b42d3cca843f6927cf7db739d494ede5c5282def
MiguelDias <miguel.dias@estudante.ufjf.br>
2024-10-18 03:34:06 -0300

    change tittle

--- src/Componentes/Header/Header.tsx
+++ src/Componentes/Header/Header.tsx
@@ -3,7 +3,7 @@ import "./header.css";
 function App() {
   return (
     <header>
-      <h1>Game Profiles Test</h1>
+      <h1>Game Profiles Identifier</h1>
       <h2>inspired in Quantic Foundry</h2>
     </header>
   );
