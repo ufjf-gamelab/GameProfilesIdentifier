0985ba5b36a5dfbf079e0aa15004b0c45f0c4653
Bebs Fiorillo <estevaovrb2014@gmail.cm>
2024-10-19 14:41:09 -0300

    iguala footer a header  e ajusta espacamento texto ref

--- src/Componentes/Footer/Footer.tsx
+++ src/Componentes/Footer/Footer.tsx
@@ -8,17 +8,17 @@ function App() {
         <img src={inova}></img>
         <section >
             <h2>Desenvolvido por:</h2>
-            <h3>Tevo Fiorilo</h3>
+            <h3>Estêvão Fiorilo</h3>
             <h3>Miguel Dias</h3> 
             <h3> Isabela Coelho</h3>        
         </section>
         <section >
           <h2>Referências:</h2>
-            <a  style={{color:'white'}}href='https://quanticfoundry.com/gamer-types/'>The 9 Quantic Gamer Types</a>     
-            <a  style={{color:'white'}}href='https://apps.quanticfoundry.com/surveys/start/gamerprofile/'>gamerprofile Quantic Foundry</a> 
-            <a  style={{color:'white'}}href='https://quanticfoundry.com/'>Quantic Foundry</a>
-            <a  style={{color:'white'}}href=' https://mud.co.uk/richard/selfware.html '>A Self of Sense</a>
-            <a  style={{color:'white'}}href='https:www.google.com/'>Nosso Texto Não lembro o titulo</a>      
+            <a  href='https://quanticfoundry.com/gamer-types/'>The 9 Quantic Gamer Types</a>     
+            <a  href='https://apps.quanticfoundry.com/surveys/start/gamerprofile/'>GamerProfile Quantic Foundry</a> 
+            <a  href='https://quanticfoundry.com/'>Quantic Foundry</a>
+            <a  href=' https://mud.co.uk/richard/selfware.html '>A Self of Sense</a>
+            <a  href='https:www.google.com/'>Nosso Texto Não lembro o titulo</a>      
 
         </section>
     </footer>
