919752e71a4bc9fe2fa7013a569754eb33fb37cd
Bebs Fiorillo <estevaovrb2014@gmail.cm>
2024-10-19 14:42:58 -0300

    arruma referencias

--- src/Componentes/Footer/Footer.tsx
+++ src/Componentes/Footer/Footer.tsx
@@ -17,8 +17,8 @@ function App() {
             <a  href='https://quanticfoundry.com/gamer-types/'>The 9 Quantic Gamer Types</a>     
             <a  href='https://apps.quanticfoundry.com/surveys/start/gamerprofile/'>GamerProfile Quantic Foundry</a> 
             <a  href='https://quanticfoundry.com/'>Quantic Foundry</a>
-            <a  href=' https://mud.co.uk/richard/selfware.html '>A Self of Sense</a>
-            <a  href='https:www.google.com/'>Nosso Texto Não lembro o titulo</a>      
+            <a  href='https://mud.co.uk/richard/selfware.html '>A Self of Sense</a>
+            <a  href='https://www2.ufjf.br/inovagames/2024/10/10/o-que-motiva-os-jogadores-e-o-que-te-motiva-a-jogar-parte-3/'>O que motiva os Jogadores? (Parte 3) - Site Inova Games</a>      
 
         </section>
     </footer>
