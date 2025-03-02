640340af92e5ed167d5a1681d576c6afaabeac4a
MiguelDias <miguel.dias@estudante.ufjf.br>
2025-03-01 22:51:05 -0300

    adiciona sliders

--- src/pages/Personas/Personas.tsx
+++ src/pages/Personas/Personas.tsx
@@ -10,12 +10,7 @@ import ResultApi from '../../Controlers/ResultGameApi.js'
 function Personas() {
   const resultApi = new ResultApi()
 
-  const [ação, setAção] = useState(resultApi.Inputs.ação)
-  const [social, setSocial] = useState(resultApi.Inputs.social)
-  const [maestria, setMaestria] = useState(resultApi.Inputs.maestria)
-  const [conquista, setConquista] = useState(resultApi.Inputs.conquista)
-  const [imersão, setImersão] = useState(resultApi.Inputs.imersão)
-  const [criatividade, setCriatividade] = useState(resultApi.Inputs.criatividade)
+  
   return (
     <div className='PersonasCtn'>
       <Header></Header>
