d05b1d0a803aa7169ff95f6997f90bf91dc7245b
MiguelDias <miguel.dias@estudante.ufjf.br>
2025-03-02 01:07:44 -0300

    ajusta disposição dos resultados

--- src/Componentes/Results/Resultado.tsx
+++ src/Componentes/Results/Resultado.tsx
@@ -1,96 +1,22 @@
-import {
-  VictoryChart,
-  VictoryArea,
-  VictoryPolarAxis,
-  VictoryTheme,
-} from "victory";
+
 import "./Resultado.css";
+import { Component } from "./Graphs/BarChart/BarChart";
+import { Button } from "@/components/ui/button";
 function Resultado(props: any) {
   const resultApi = props.resultApi;
-  let categorias = resultApi.Nomes;
-  resultApi.Inputs.ação = props.ação;
-  resultApi.Inputs.social = props.social;
-  resultApi.Inputs.maestria = props.maestria;
-  resultApi.Inputs.conquista = props.conquista;
-  resultApi.Inputs.imersão = props.imersão;
-  resultApi.Inputs.criatividade = props.criatividade;
+
   
   const ResultadoTexto = resultApi.getTextoResultado();
   return (
     <div className="ResultadoCtn">
       <h2>Resultado</h2>
       <div>
-        <VictoryChart polar theme={VictoryTheme.material}>
-          {categorias?.map((nome: any, i: any) => {
-            return (
-              <VictoryPolarAxis
-                dependentAxis
-                key={i}
-                
-                label={nome + '\n' + resultApi.Agradabilidades[i].agradabilidade.toFixed(0)+"%" }
-                labelPlacement="perpendicular"
-                style={{ tickLabels: { fill: "none" } }}
-                axisValue={nome}
-              />
-            );
-          })}
-          <VictoryArea
-            style={{ data: { fill: "#00000080", width: 100 } }}
-            data={[
-              {
-                x: categorias[0],
-                y: resultApi.Agradabilidades[0].agradabilidade,
-              },
-              {
-                x: categorias[1],
-                y: resultApi.Agradabilidades[1].agradabilidade,
-              },
-              {
-                x: categorias[2],
-                y: resultApi.Agradabilidades[2].agradabilidade,
-              },
-              {
-                x: categorias[3],
-                y: resultApi.Agradabilidades[3].agradabilidade,
-              },
-              {
-                x: categorias[4],
-                y: resultApi.Agradabilidades[4].agradabilidade,
-              },
-              {
-                x: categorias[5],
-                y: resultApi.Agradabilidades[5].agradabilidade,
-              },
-              {
-                x: categorias[6],
-                y: resultApi.Agradabilidades[6].agradabilidade,
-              },
-              {
-                x: categorias[7],
-                y: resultApi.Agradabilidades[7].agradabilidade,
-              },
-              {
-                x: categorias[8],
-                y: resultApi.Agradabilidades[8].agradabilidade,
-              },
-            ]}
-          />
-        </VictoryChart>
+        <Component></Component>
       </div>
-      <div className="resultText">
-        <br />
-        {ResultadoTexto.nomeMaiorElemento !== "" ? (
-          
-          <>
-            {"O perfil que mais gostaria do seu jogo é: "}
-            <br />
-            <br />
-            {"•⁠ "+ResultadoTexto.nomeMaiorElemento + ": " + ResultadoTexto.textoPrimeiro}
-            <br />
-            <br />
-            {"•⁠ "+ResultadoTexto.nomeSegundoMaiorElemento + ": " + ResultadoTexto.textoSegundo}
-          </>
-        ) : null}
+      <div className="OperationsPanel">
+          <Button onClick={() => {resultApi.saveResult()}}>Salvar</Button>
+          <Button onClick={() => {resultApi.saveResult()}}>Salvar</Button>
+          <Button onClick={() => {resultApi.saveResult()}}>Salvar</Button>
       </div>
     </div>
   );
