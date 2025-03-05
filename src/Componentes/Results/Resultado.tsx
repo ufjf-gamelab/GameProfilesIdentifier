a362b31df25be698b339d80991184f84c20a16a3
MiguelDias <miguel.dias@estudante.ufjf.br>
2025-03-04 22:37:21 -0300

    adiciona sistema de tabs para multiplas analises

--- src/Componentes/Results/Resultado.tsx
+++ src/Componentes/Results/Resultado.tsx
@@ -3,6 +3,10 @@ import "./Resultado.css";
 import { Component } from "./Graphs/BarChart/BarChart";
 import { Button } from "@/components/ui/button";
 import { treeData } from "@/Controlers/TreeApi";
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@radix-ui/react-tabs";
+import gameFeature from "../gameFeaturesPicker/gameFeature/gameFeature";
+import GameFeaturesPicker from "../gameFeaturesPicker/GameFeaturesPicker";
+import PersonasTree from "../PersonasTree/PersonasTree";
 
 type ResultadoProps = {
   arvore: treeData
@@ -14,20 +18,26 @@ function Resultado({arvore}: ResultadoProps) {
   return (
     <div className="ResultadoCtn">
       <h2>Resultado</h2>
-      <div className="Graphs">
-        <Component
-          data={
-            arvore.gameValtoData()
-          }
-          titulo="Resultado"
-          descricao={"ResultadoTexto"}
-        ></Component>
-      </div>
-      <div className="OperationsPanel">
-          <Button >Salvar</Button>
-          <Button >Salvar</Button>
-          <Button >Salvar</Button>
-      </div>
+      <Tabs defaultValue="account" className="tabLayouyt">
+       
+        <div className="Graphs">
+          <TabsContent className="space-y-2" value="account">
+            <Component
+              data={arvore.gameValtoData()}
+              titulo="Resultado"
+              descricao={"ResultadoTexto"}
+            ></Component>
+          </TabsContent>
+        </div>
+          <TabsList className="OperationsPanel">
+            <TabsTrigger className="TabBtn" value="account">Ver Jogo</TabsTrigger>
+            <TabsTrigger className="TabBtn" value="password">Tree</TabsTrigger>
+          </TabsList>
+     
+      </Tabs>
+      
+         
+     
     </div>
   );
 }
