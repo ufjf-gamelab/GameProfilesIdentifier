0e985a0fbd0de70fd87acbe45396c40f4792f2be
Miguel-Dias-112 <miguel.dias@estudante.ufjf.br>
2025-03-16 15:05:23 -0300

    define actions api

--- src/Controlers/TreeApi.ts
+++ src/Controlers/TreeApi.ts
@@ -3,238 +3,171 @@ import { actions } from 'react-arborist/dist/module/state/open-slice';
 import { v4 as uuidv4 } from 'uuid';
 import { GameFeatureProps } from './Types';
 
-export type Categorias={
-    Community: number,
-    Competition: number,
-    Excitement: number,
-    Destruction: number,
-    Completion: number,
-    Power: number,
-    Strategy: number,
-    Challenge: number,
-    Fantasy: number,
-    Story: number,
-    Discovery: number,
-    Design: number,
-  }
+export type Categorias = {
+  Community: number;
+  Competition: number;
+  Excitement: number;
+  Destruction: number;
+  Completion: number;
+  Power: number;
+  Strategy: number;
+  Challenge: number;
+  Fantasy: number;
+  Story: number;
+  Discovery: number;
+  Design: number;
+};
+
 export type Motivações = {
-    ação:number,
-    social : number,
-    maestria : number,
-    conquista : number,
-    imersão : number,
-    criatividade : number
-  };
+  ação: number;
+  social: number;
+  maestria: number;
+  conquista: number;
+  imersão: number;
+  criatividade: number;
+};
+
 export type Persona = {
-  nome: string,
-  imagem: string,
-  descricao: string,
-  categoria: string,
-  quantidade: number,
-  pesos:Categorias
-}
+  nome: string;
+  imagem: string;
+  descricao: string;
+  categoria: string;
+  quantidade: number;
+  pesos: Categorias;
+};
+
 export type Jogo = {
-    nome: string,
-    imagem: string,
-    descricao: string,
-    categoria: string,
-    quantidade: number,
-    Valores: Motivações,
-  }
-export type Data={
-  subtitle: string,
-  dataKey: number
-}
+  nome: string;
+  imagem: string;
+  descricao: string;
+  categoria: string;
+  quantidade: number;
+  Valores: Motivações;
+};
 
+export type Data = {
+  subtitle: string;
+  dataKey: number;
+};
 
-export class treeData{
+export class TreeData {
   id: string;
   name: string;
   pesos: Motivações;
-  children: treeData[];
-  constructor( name: string, pesos: Motivações, children: treeData[]){
-    this.id= uuidv4(),
+  children: TreeData[];
+
+  constructor(name: string, pesos: Motivações, children: TreeData[] = []) {
+    this.id = uuidv4();
     this.name = name;
     this.pesos = pesos;
     this.children = children;
-
-
   }
-  gameValtoData(){
-    return [
-      { subtitle: "Ação", dataKey: this.pesos.ação },
-      { subtitle: "Social", dataKey: this.pesos.social },
-      { subtitle: "Maestria", dataKey: this.pesos.maestria },
-      { subtitle: "Conquista", dataKey: this.pesos.conquista },
-      { subtitle: "Imersão", dataKey: this.pesos.imersão },
-      { subtitle: "Criatividade", dataKey: this.pesos.criatividade },
-    ]
-  }
-  
 
+  gameValtoData() {
+    return Object.keys(this.pesos).map((key) => ({
+      subtitle: key.charAt(0).toUpperCase() + key.slice(1),
+      dataKey: this.pesos[key as keyof Motivações],
+    }));
+  }
 }
-export class PersonasTreeApi{
-  tree: treeData[] = [new treeData(
-    "Jogo",
-    { ação: 0, social: 0, maestria: 0, conquista: 0, imersão: 0, criatividade: 0 },
-    []
-  )];
-  nosSelecionados: treeData[] = [];
-  constructor( ){
-    this.nosSelecionados .push(this.tree[0]);
-  }
-  areSelected(uuid: String):boolean{
-    return this.nosSelecionados.find((item) => item.id === uuid) ? true : false;
-  }
-  findbyUUID(node: any, targetUUID: String): any {
-    // Se o nó atual tiver o UUID que procuramos, retorna o nó
-    console.log("achei",node.id,targetUUID)
 
+export class PersonasTreeApi {
+  tree: TreeData[] = [new TreeData("Jogo", { ação: 0, social: 0, maestria: 0, conquista: 0, imersão: 0, criatividade: 0 })];
+  nosSelecionados: TreeData[] = [];
+
+  constructor() {
+    this.nosSelecionados.push(this.tree[0]);
+  }
+
+  areSelected(uuid: string): boolean {
+    return this.nosSelecionados.some((item) => item.id === uuid);
+  }
+
+  findbyUUID(node: TreeData, targetUUID: string): TreeData | null {
     if (node.id === targetUUID) {
       return node;
     }
-    // Se o nó tiver filhos, percorre cada um
+
     if (node.children) {
       for (let child of node.children) {
         const result = this.findbyUUID(child, targetUUID);
-        if (result) {
-          return result;
-        }
+        if (result) return result;
       }
     }
-  
-    // Se não encontrar o UUID, retorna null
+
     return null;
   }
-  compartiveDataSet():any{
 
+  compartiveDataSet() {
     const actions: (keyof Motivações)[] = ["ação", "social", "maestria", "conquista", "imersão", "criatividade"];
     const dataSet: { dataKeys: string[], data: { [key: string]: any }[] } = { dataKeys: [], data: [] };
-    actions.forEach((legenda) => {
-        const data: { [key: string]: any } = { subtitle: legenda };
-        let name = "";
-        this.nosSelecionados.forEach((node,index) => {
-          
-          data[node.name] = node.pesos[legenda];
-          name = node.name;
-        });
-        dataSet["data"].push(data);
-        return data;
-    });
-    dataSet.dataKeys =  this.nosSelecionados.map((node) => node.name);
-    console.log(dataSet);
-    return dataSet;
 
+    actions.forEach((legenda) => {
+      const data: { [key: string]: any } = { subtitle: legenda };
+      this.nosSelecionados.forEach((node) => {
+        data[node.name] = node.pesos[legenda];
+      });
+      dataSet.data.push(data);
+    });
+
+    dataSet.dataKeys = this.nosSelecionados.map((node) => node.name);
+    console.log(dataSet);
+
+    return dataSet;
   }
-  addPersona( valor: String) {
-    this.tree[0].children.push(
-      {
-      id: uuidv4(),
-      name: valor,
-      children:[],
-      pesos: { ação: 3, social: 3, maestria: 3, conquista: 3, imersão: 3, criatividade: 3 }
-    }as never
-    );      
-} 
+
+  addPersona(valor: string) {
+    const newPersona = new TreeData(valor, {
+      ação: 3, social: 3, maestria: 3, conquista: 3, imersão: 3, criatividade: 3
+    });
+    this.tree[0].children.push(newPersona);
+  }
 }
+
 export function selectEditedNode(personasTreeApi: PersonasTreeApi) {
-  const justOne = personasTreeApi.nosSelecionados.length == 1
-  if (justOne) {
-    return personasTreeApi.nosSelecionados[0];
-  } else {
-    return undefined;
-  }
+  return personasTreeApi.nosSelecionados.length === 1 ? personasTreeApi.nosSelecionados[0] : undefined;
 }
 export function TreeReducer(state: any, action: any) {
   const novoEstado = cloneWithMethods(state);
   const no = novoEstado.findbyUUID(state.tree[0], selectEditedNode(state)?.id);
+
+  const updatePesos = (key: keyof Motivações, value: number) => {
+    if (no) {
+      no.pesos[key] = value;
+    }
+  };
+
   switch (action.type) {
     case "ADD_PERSONA":
       state.addPersona(action.value);
-      return novoEstado
+      return novoEstado;
     case "ADD_SELECTED_NODE":
       const node = novoEstado.findbyUUID(novoEstado.tree[0], action.uuid);
-      novoEstado.nosSelecionados.push(node);
-      return  novoEstado
+      if (node) novoEstado.nosSelecionados.push(node);
+      return novoEstado;
     case "REMOVE_SELECTED_NODE":
-      novoEstado.nosSelecionados = novoEstado.nosSelecionados.filter(
-        (item: any) => item.id !== action.uuid
-      );
-      return novoEstado
-    case "SET_ACTION":
-      no.pesos['ação'] = action.value; 
+      novoEstado.nosSelecionados = novoEstado.nosSelecionados.filter((item: any) => item.id !== action.uuid);
+      return novoEstado;
+    case "SET_ACAO":
+      updatePesos("ação", action.value);
       return novoEstado;
     case "SET_SOCIAL":
-      no.pesos['social'] = action.value; 
+      updatePesos("social", action.value);
       return novoEstado;
-      case "SET_MAESTRIA":
-        no.pesos['maestria'] = action.value; 
-        return novoEstado;
+    case "SET_MAESTRIA":
+      updatePesos("maestria", action.value);
+      return novoEstado;
     case "SET_CONQUISTA":
-      no.pesos['conquista'] = action.value; 
+      updatePesos("conquista", action.value);
       return novoEstado;
     case "SET_IMERSÃO":
-      no.pesos['imersão'] = action.value;
+      updatePesos("imersão", action.value);
       return novoEstado;
     case "SET_CRIATIVIDADE":
-      no.pesos['criatividade'] = action.value;
+      updatePesos("criatividade", action.value);
       return novoEstado;
     default:
       return state;
   }
 }
 
-
-export function getActions(selectedNode:treeData,dispatch:React.Dispatch<any>):GameFeatureProps[]{
-  const gameFeature: GameFeatureProps[] = [
-    {
-      textLabel: "Ação",
-      textdescription: "Foco em destruição e excitação intensa.",
-      setValue: (value: number) => {
-        dispatch({ type: "SET_ACTION", value });
-      },
-      valorProp: selectedNode?.pesos.ação!,
-    },
-    {
-      textLabel: "Social",
-      textdescription: "Competição e interação em comunidade.",
-      setValue: (value: number) => {
-        dispatch({ type: "SET_SOCIAL", value });
-      },
-      valorProp: selectedNode?.pesos.social!,
-    },
-    {
-      textLabel: "Maestria",
-      textdescription: "Desafio e desenvolvimento de estratégias",
-      setValue: (value: number) => {
-        dispatch({ type: "SET_MAESTRIA", value });
-      },
-      valorProp: selectedNode?.pesos.maestria!,
-    },
-    {
-      textLabel: "Conquista",
-      textdescription: "Completar objetivos e obter poder.",
-      setValue: (value: number) => {
-        dispatch({ type: "SET_CONQUISTA", value });
-      },
-      valorProp: selectedNode?.pesos.conquista!,
-    },
-    {
-      textLabel: "Imersão",
-      textdescription: "Exploração de fantasia e histórias profundas",
-      setValue: (value: number) => {
-        dispatch({ type: "SET_IMERSÃO", value });
-      },
-      valorProp: selectedNode?.pesos.imersão!,
-    },
-    {
-      textLabel: "Criatividade",
-      textdescription: "Personalização e descoberta de novidades.",
-      setValue: (value: number) => {
-        dispatch({ type: "SET_CRIATIVIDADE", value });
-      },
-      valorProp: selectedNode?.pesos.criatividade!,
-    },
-  ];
-  return gameFeature;
-}
