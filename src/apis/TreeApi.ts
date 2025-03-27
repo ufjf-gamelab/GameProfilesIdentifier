c38241dd0ea5629cb2e4c8518c3a68823a74454e
Miguel-Dias-112 <miguel.dias@estudante.uffj.br>
2025-03-26 22:55:46 -0300

    move useReduce pra fora da Api da da arvore

--- src/apis/TreeApi.ts
+++ src/apis/TreeApi.ts
@@ -118,57 +118,7 @@ export class PersonasTreeApi {
 //   //   : undefined;
 
 // }
-export function TreeReducer(state: any, action: any) {
-  const novoEstado = cloneWithMethods(state);
-  const no = state.noEmEdicao;
 
-  const updatePesos = (key: keyof Motivações, value: number) => {
-    if (no) {
-      no.pesos[key] = value;
-    }
-  };
-
-  switch (action.type) {
-    case "ADD_PERSONA":
-      state.addPersona(action.value);
-      return novoEstado;
-    case "ADD_SELECTED_NODE":
-      const node = novoEstado.findbyUUID(novoEstado.tree[0], action.value);
-      if (node) novoEstado.nosSelecionados.push(node);
-      return novoEstado;
-    case "REMOVE_SELECTED_NODE":
-
-      novoEstado.nosSelecionados = novoEstado.nosSelecionados.filter(
-        (item: any) => {
-          return item.id !== action.value
-        }
-      );
-      return novoEstado;
-    case "MUDA_EDITABLE_NODE":
-      novoEstado.noEmEdicao = novoEstado.findbyUUID(novoEstado.tree[0], action.value);
-      return novoEstado;
-    case "SET_AÇÃO":
-      updatePesos("ação", action.value);
-      return novoEstado;
-    case "SET_SOCIAL":
-      updatePesos("social", action.value);
-      return novoEstado;
-    case "SET_MAESTRIA":
-      updatePesos("maestria", action.value);
-      return novoEstado;
-    case "SET_CONQUISTA":
-      updatePesos("conquista", action.value);
-      return novoEstado;
-    case "SET_IMERSÃO":
-      updatePesos("imersão", action.value);
-      return novoEstado;
-    case "SET_CRIATIVIDADE":
-      updatePesos("criatividade", action.value);
-      return novoEstado;
-    default:
-      return state;
-  }
-}
 
 
 export type { Motivações };
