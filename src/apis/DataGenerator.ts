50cf2cba0ddf099948117bb8fb603caf4e1100a9
Miguel-Dias-112 <miguel.dias@estudante.uffj.br>
2025-03-27 00:39:15 -0300

    cria tipo DataSet

--- src/apis/DataGenerator.ts
+++ src/apis/DataGenerator.ts
@@ -1,6 +1,8 @@
 import { Motivações, PersonasTreeApi } from "./TreeApi";
-import { TreeData } from "./Types/PersonasTreeType";
-
+type dataSet ={
+    dataKeys:string[]
+    data : { [key: string]: any } 
+}
 export class DataGenerator {
     #arvoreAnalisada: PersonasTreeApi;
     #motivacoesKeywords: (keyof Motivações)[] = [
@@ -16,11 +18,10 @@ export class DataGenerator {
     }
     getAbsoluteDataSet() {
         const nosSelecionados = this.#arvoreAnalisada.nosSelecionados
-        const dataSet: { dataKeys: string[]; data: { [key: string]: any }[] } = {
+        const dataSet:dataSet = {
             dataKeys: [],
             data: [],
         };
-
         this.#motivacoesKeywords.forEach((legenda) => {
             const data: { [key: string]: any } = { subtitle: legenda };
             nosSelecionados.forEach((node) => {
@@ -28,27 +29,23 @@ export class DataGenerator {
             });
             dataSet.data.push(data);
         });
-
         dataSet.dataKeys = nosSelecionados.map((node) => node.name);
-        console.log(dataSet);
-
         return dataSet;
     }
     getAvaregeDataSet() {
-        
         const jogo = this.#arvoreAnalisada.jogo
         const nosSelecionados = this.#arvoreAnalisada.nosSelecionados
         const nosAnalisados = nosSelecionados.filter((node) => {
             if (node.name === "Jogo") return false;
             return true;
         });
-        const dataSet: { dataKeys: string[]; data: { [key: string]: any }[] } = {
+        const dataSet:dataSet= {
             dataKeys: [],
             data: [],
         };
         this.#motivacoesKeywords.forEach((legenda) => {
-            const data: { [key: string]: any } = { subtitle: legenda };
             let totalPesos = 0;
+            const data: { [key: string]: any } = { subtitle: legenda };
             nosAnalisados.forEach((node) => {
                 console.log(node.pesos[legenda]);
                 totalPesos += node.pesos[legenda];
