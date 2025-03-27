d5f5498a5ad35b5b96bdfd9c424e6c37a8aceef7
Miguel-Dias-112 <miguel.dias@estudante.uffj.br>
2025-03-26 22:28:51 -0300

    remove pages e unifica aplicação no App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,55 @@
-
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
-import ResultApi from './Controlers/QFoundryApi.js'
-import GameCreator from './pages/GameCreator/GameCreator.js'
+import "./App.css";
+import Header from "./Componentes/Header/Header.tsx";
+import Footer from "./Componentes/Footer/Footer.tsx"
+import GameFeaturesPicker from "./Componentes/gameFeaturesPicker/GameFeaturesPicker.tsx";
+import Resultado from "./Componentes/Results/Resultado.tsx";
+import { useReducer } from "react";
+import PersonasTree from "@/Componentes/PersonasTree/PersonasTree.tsx";
+import {
+  PersonasTreeApi,
+  TreeReducer,
+} from "@/Controlers/TreeApi.ts";
+import { getActions } from "@/Controlers/ActionsApi.tsx";
 
 function App() {
-
- 
+  const [estado, dispatch] = useReducer(TreeReducer, new PersonasTreeApi());
+  const selectedNode = estado.noEmEdicao;
+  const actions =  getActions(selectedNode!, dispatch);
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<GameCreator />}>
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  )
-}
+        <div className="GameCreatorCtn">
+          <Header></Header>
+            <main>
+              <aside>
+                <PersonasTree
+                  addPersonaHandler={(value) => {
+                    dispatch({ type: "ADD_PERSONA", value });
+                  }}
+                  pushNosSelecionados={(value) => {
+                    dispatch({ type: "ADD_SELECTED_NODE", value });
+                  }}
+                  removeNosSelecionados={(value) => {
+                    dispatch({ type: "REMOVE_SELECTED_NODE", value });
+                  }}
+                  mudaNoEditavel={(value) => {
+                    dispatch({ type: "MUDA_EDITABLE_NODE", value})
+                  }}
+                  arvoreApi={estado}
+                />
+              </aside>
+                
+              <div className="Results">
+                <Resultado personasTree={estado} />
+              </div>
 
-export default App
+              <aside>
+                <GameFeaturesPicker
+                  Features={actions}
+                  disabled={selectedNode === undefined}
+                />
+              </aside>
+            </main>
+            <Footer></Footer>
+        </div> 
+  );
+}
+export default App;
