717caa16a07f8b382065436acdebd9f0a09a5e50
Miguel-Dias-112 <miguel.dias@estudante.uffj.br>
2025-03-31 15:33:41 -0300

    readd victory chart da quantic foundry

--- src/components/created/Results/Resultado.tsx
+++ src/components/created/Results/Resultado.tsx
@@ -1,5 +1,5 @@
 import "./Resultado.css";
-import {  PersonasApi } from "@/apis/PersonasApi";
+import { PersonasApi } from "@/apis/PersonasApi";
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@radix-ui/react-tabs";
 
 import { ComparativeChart } from "./Graphs/ComparitiveChart/ComparativeChart";
@@ -10,82 +10,74 @@ import { Button } from "@/components/ui/button";
 
 type ResultadoProps = {
   personasTree: PersonasApi;
-  actualResult:"Comparative" | "Media" | "qFoundry" | "steamGames";
+  actualResult: "Comparative" | "Media" | "qFoundry" | "steamGames";
   setActualResult: (value: "Comparative" | "Media" | "qFoundry" | "steamGames") => void;
 };
 
-function Resultado({ personasTree, actualResult,setActualResult }: ResultadoProps) {
+function Resultado({ personasTree, actualResult, setActualResult }: ResultadoProps) {
   const dataGenerator = new DataGenerator(personasTree);
   const abosoluteDataset = dataGenerator.getAbsoluteDataSet();
   const avaregeDataSet = dataGenerator.getAvaregeDataSet();
   const gameValues = personasTree.arvorePersonas[0].pesos;	
 
-  
-  
-   
   return (
     <div className="ResultadoCtn">
       <h2>Resultado</h2>
       <div className="TabLayout">
-          {!!abosoluteDataset && (
-            <div className="=graphs">
-              {actualResult === "Comparative" && (
-                <ComparativeChart
-                  chartData={abosoluteDataset.data}
-                  titulo="Comparativo"
-                  personasNomes={abosoluteDataset.dataKeys}
-                ></ComparativeChart>
-              )}
-              {actualResult === "Media" && (
-                <ComparativeChart
-                  chartData={avaregeDataSet.data}
-                  titulo="Comparativo"
-                  personasNomes={avaregeDataSet.dataKeys}
-                ></ComparativeChart>
-              )}
-              {actualResult === "qFoundry" && (
-                <ComparativeChart
-                  chartData={abosoluteDataset.data}
-                  titulo="Comparativo"
-                  personasNomes={abosoluteDataset.dataKeys}
-                ></ComparativeChart>
-              )}
-              {actualResult === "steamGames" && (
-                <SteamGameApi
-                   jogo={personasTree.arvorePersonas[0]}
-                ></SteamGameApi>
-              )}
-
-            </div>
+        {!!abosoluteDataset && (
+          <div className="=graphs">
+            {actualResult === "Comparative" && (
+              <ComparativeChart
+                chartData={abosoluteDataset.data}
+                titulo="Comparativo"
+                personasNomes={abosoluteDataset.dataKeys}
+              ></ComparativeChart>
             )}
+            {actualResult === "Media" && (
+              <ComparativeChart
+                chartData={avaregeDataSet.data}
+                titulo="Comparativo"
+                personasNomes={avaregeDataSet.dataKeys}
+              ></ComparativeChart>
+            )}
+            {actualResult === "qFoundry" && (
+              <VictoryChart
+                gameValues={gameValues}
+              ></VictoryChart>
+            )}
+            {actualResult === "steamGames" && (
+              <SteamGameApi
+                jogo={personasTree.arvorePersonas[0]}
+              ></SteamGameApi>
+            )}
+
+          </div>
+        )}
       </div>
-         
-        
-    
-        <div className="OperationsPanel">
-            <Button className="TabBtn" variant="outline" onClick={() => {
-              setActualResult("Comparative");
-            }}>
-              Comparativo
-            </Button>
-            <Button className="TabBtn" variant="outline" onClick={() => {
-              setActualResult("Media");
-            }}>
-              Média
-            </Button>
-            <Button className="TabBtn" variant="outline" onClick={() => {
-              setActualResult("qFoundry");
-            }}>
-              Quantic Foundry
-            </Button>
-            <Button className="TabBtn" variant="outline" onClick={() => {
-              setActualResult("steamGames");
-            }
-            }>
-              Steam Games
-            </Button>
-        </div>
-        
+      <div className="OperationsPanel">
+        <Button className="TabBtn" variant="outline" onClick={() => {
+          setActualResult("Comparative");
+        }}>
+          Comparativo
+        </Button>
+        <Button className="TabBtn" variant="outline" onClick={() => {
+          setActualResult("Media");
+        }}>
+          Média
+        </Button>
+        <Button className="TabBtn" variant="outline" onClick={() => {
+          setActualResult("qFoundry");
+        }}>
+          Quantic Foundry
+        </Button>
+        <Button className="TabBtn" variant="outline" onClick={() => {
+          setActualResult("steamGames");
+        }
+        }>
+          Steam Games
+        </Button>
+      </div>
+
     </div>
   );
 }
