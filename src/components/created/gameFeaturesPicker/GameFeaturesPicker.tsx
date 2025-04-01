5aae31adc141b9269ae0e468aa13abfd4c2d9b75
Miguel-Dias-112 <miguel.dias@estudante.uffj.br>
2025-04-01 16:59:41 -0300

    ajusta menu de botões

--- src/components/created/gameFeaturesPicker/GameFeaturesPicker.tsx
+++ src/components/created/gameFeaturesPicker/GameFeaturesPicker.tsx
@@ -23,19 +23,19 @@ function GameFeaturesPicker({ Features,disabled,namePersona,editPersonaName,most
           </section>
 
         </section>
-        <section >
+        <section className="GameSelectCtn__btns">
 
           <Button onClick={
             () => mostrarMediaFilhos()
           }>
-            mostrar media dos filhos
+            media dos filhos
           </Button>
           <Button onClick={
             () => mostrarSomaFilhos()
           }>
-            mostrar soma dos filhos
+            soma dos filhos
           </Button>
-          <Button onClick={() => mostrarNoEmEdicao()}>mostrar valores</Button>
+          <Button onClick={() => mostrarNoEmEdicao()}>valores absolutos</Button>
         </section>
 
         <section>
